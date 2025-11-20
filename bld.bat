b87477d3efa4bffadea83c2d1ecf11406dd698b0
fermarsan <fmartinezsanta@gmail.com>
2025-11-20 08:51:11 -0500

    update scripts

--- bld.bat
+++ bld.bat
@@ -5,6 +5,7 @@ xcopy win-64\aixt\aixt.exe %PREFIX%\aixt /E /I
 xcopy win-64\aixt\docs %PREFIX%\aixt\docs /E /I
 xcopy win-64\aixt\lib %PREFIX%\aixt\lib /E /I
 xcopy win-64\aixt\setup %PREFIX%\aixt\setup /E /I
+xcopy win-64\aixt\src %PREFIX%\aixt\src /E /I
 xcopy win-64\aixt\targets %PREFIX%\aixt\targets /E /I
 xcopy win-64\aixt\templates %PREFIX%\aixt\templates /E /I
 
