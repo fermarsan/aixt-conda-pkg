966aa14979e73248e76314dd8fbff8ea9f8e7089
fermarsan <fmartinezsanta@gmail.com>
2025-11-20 00:40:47 -0500

    update

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ cp linux-64/aixt/aixt $PREFIX/aixt/
 cp -r linux-64/aixt/docs $PREFIX/aixt/
 cp -r linux-64/aixt/lib $PREFIX/aixt/
 cp -r linux-64/aixt/setup $PREFIX/aixt/
-cp -r linux-64/aixt/src $PREFIX/aixt/
 cp -r linux-64/aixt/targets $PREFIX/aixt/
 cp -r linux-64/aixt/templates $PREFIX/aixt/
 
